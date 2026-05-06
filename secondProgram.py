b7d3cc1444bcbeb2bac9d424d88b28cf720d0f5d
lucky-23 <sololeveling2684@gmail.com>
2026-05-06 13:49:40 +0545

    Initial commit

---
+++ secondProgram.py
@@ -0,0 +1,64 @@
+ # Iterating over a list
+fruits = ["apple", "banana", "cherry"]
+ for fruit in fruits:
+     print(fruit)
+
+ # Iterating over a string
+ name = "Python"
+ for char in name:
+     print(char)
+
+ # Iterating using range()
+ for i in range(5):  # Iterates from 0 to 4
+     print(i)
+
+ for j in range(2, 7): # Iterates from 2 to 6
+     print(j)
+
+ for k in range(1, 10, 2): # Iterates from 1 to 9, stepping by 2
+     print(k)
+     # Iterating over a list
+ fruits = ["apple", "banana", "cherry"]
+ for fruit in fruits:
+     print(fruit)
+
+ # Iterating over a string
+ name = "Python"
+ for char in name:
+     print(char)
+
+ # Iterating using range()
+ for i in range(5):  # Iterates from 0 to 4
+     print(i)
+
+ for j in range(2, 7): # Iterates from 2 to 6
+     print(j)
+
+ for k in range(1, 10, 2): # Iterates from 1 to 9, stepping by 2
+     print(k)
+
+
+ for i in range (5):
+     print (i)
+ for j in range (2,7):
+     print (j)
+
+# # for k in range(1,10,2):
+# #     print(k)
+# a = 12
+# b = 25
+# d =55.25
+# d -=15
+# print ("d:",d)
+# c=d
+# c+=15.25
+# print("c:",c)
+# e=c
+# e/=5
+# print ("e:",e)
+
+num=10
+num= num+10
+print("num:",num)#      20 answer
+
+num+=10  #answer20
