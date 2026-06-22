0a8072744b10992ac53b7aa5c98253e268e98b5b
lucky-23 <sololeveling2684@gmail.com>
2026-06-22 16:05:09 +0545

    Add initial HTML structure and update JavaScript variable assignments

--- ours/script.js
+++ ours/script.js
@@ -1,2 +1,33 @@
-document.getElementById("help").innerHTML =
-"Once upon a time...";
+// let a = true;
+// let b = false;
+
+// console.log(`rita is a boy : ${b}`);
+
+
+
+// // let a = 15
+
+// // let c = true
+
+// // console.log(a);
+// // console.log(c)
+
+
+// let name = "Harry"
+// let age = 25
+// let gender = "male"
+
+// console.log(`the name of this person is ${name} his gender is ${gender} and his age is ${age}`);
+// document.getElementById("name").innerText = name;
+// document.getElementById("gender").innerText = gender;
+// document.getElementById("age").innerText = age;
+
+// document.getElementById("name").innerText = (`he is ${name}`);
+// document.getElementById("gender").innerText = (`his gender is ${gender}`);
+// document.getElementById("age").innerText = (`his age is ${age}`);
+
+let a = "true"
+let b = "false"
+
+document.getElementById("a").innerText = a;
+document.getElementById("b").innerText = b;
