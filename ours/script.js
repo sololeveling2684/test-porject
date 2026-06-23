23435186835909c73a4ad5077534fbcc6f0924bc
lucky-23 <sololeveling2684@gmail.com>
2026-06-23 16:00:20 +0545

    Comment out all JavaScript code in script.js for debugging purposes

--- ours/script.js
+++ ours/script.js
@@ -1,33 +1,79 @@
-// let a = true;
-// let b = false;
+// // // let a = true;
+// // // let b = false;
 
-// console.log(`rita is a boy : ${b}`);
-
-
-
-// // let a = 15
-
-// // let c = true
-
-// // console.log(a);
-// // console.log(c)
+// // // console.log(`rita is a boy : ${b}`);
 
 
-// let name = "Harry"
-// let age = 25
-// let gender = "male"
 
-// console.log(`the name of this person is ${name} his gender is ${gender} and his age is ${age}`);
-// document.getElementById("name").innerText = name;
-// document.getElementById("gender").innerText = gender;
-// document.getElementById("age").innerText = age;
+// // // // let a = 15
 
-// document.getElementById("name").innerText = (`he is ${name}`);
-// document.getElementById("gender").innerText = (`his gender is ${gender}`);
-// document.getElementById("age").innerText = (`his age is ${age}`);
+// // // // let c = true
 
-let a = "true"
-let b = "false"
+// // // // console.log(a);
+// // // // console.log(c)
 
-document.getElementById("a").innerText = a;
-document.getElementById("b").innerText = b;
+
+// // // let name = "Harry"
+// // // let age = 25
+// // // let gender = "male"
+
+// // // console.log(`the name of this person is ${name} his gender is ${gender} and his age is ${age}`);
+// // // document.getElementById("name").innerText = name;
+// // // document.getElementById("gender").innerText = gender;
+// // // document.getElementById("age").innerText = age;
+
+// // // document.getElementById("name").innerText = (`he is ${name}`);
+// // // document.getElementById("gender").innerText = (`his gender is ${gender}`);
+// // // document.getElementById("age").innerText = (`his age is ${age}`);
+
+// // let a = "true"
+// // let b = "false"
+
+// // document.getElementById("a").innerText = a;
+// // document.getElementById("b").innerText = b;
+
+// // let x = 1;
+// // let x = 2;
+
+// function function1 () {
+// let  = 15
+// console.log(a);
+// }
+// function function2 () {
+// let a = 15
+// console.log(a);
+// }
+// function1();
+// function2();
+
+// let z= 200;
+// function function3 () {
+//     let z = 300;
+// console.log(z);
+// }
+// function function4 () {
+// console.log(z);
+// }
+// function3()
+
+// var a = 15;
+// function function3 () {
+//     var a = 30;
+//     console.log(a);
+// }
+// function3(); 
+
+// var a = 50;
+// function funcction1() {
+//     let a = 30;
+// }
+
+// let a = 1;
+// let b = 2;
+// let c = a/b
+// console.log(c)
+
+// let d = 5;
+// let e = d*5;
+// console.log(e)
+
