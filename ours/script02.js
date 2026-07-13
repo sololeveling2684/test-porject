769b5d57cb7b65a15838081436af3fb9115c37cf
lucky-23 <sololeveling2684@gmail.com>
2026-07-13 22:27:47 +0545

    Update car object properties: change color, delete model, and add make

--- ours/script02.js
+++ ours/script02.js
@@ -230,3 +230,9 @@ const car = {
 }
 
 console.log(car.color); // Output: red
+car.color = "blue";
+console.log(car.color); // Output: blue
+delete car.model;
+console.log(car.model); // Output: undefined
+car.make = "Honda";
+console.log(car.make); // Output: Honda
